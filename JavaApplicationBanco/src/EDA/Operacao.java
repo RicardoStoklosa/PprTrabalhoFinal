30b2c8972d6276fc6f38ffa6ea00fdfdfdcd4e44
RicardoStoklosa <stoklosa.ricardo@gmail.com>
2017-12-07 17:17:46 -0200

    testes

--- JavaApplicationBanco/src/EDA/Operacao.java
+++ JavaApplicationBanco/src/EDA/Operacao.java
@@ -1,5 +1,58 @@
 package EDA;
 
+
+/**
+ * @author UDESC
+ */
+public class Operacao {
+    private boolean status;
+    private StringBuilder erro;
+    
+    public Operacao( ){
+        status = true;
+    }
+
+    /**
+     * @return the status
+     */
+    public boolean getStatus() {
+        return status;
+    }
+
+    /**
+     * @return the erro
+     */
+    public StringBuilder getErro() {
+        return erro;
+    }
+
+    /**
+     * Limpa o texto de erro.
+     */
+    public void limparErro(){
+        status = true;
+        erro = new StringBuilder();
+    }
+    
+    /**
+     * Anexa erro na string de erro(s).
+     * @param err define um novo erro
+     */
+    public void anexarErro(String err) {
+        status = false;
+        if( erro == null )
+            erro = new StringBuilder();
+        else
+            erro.append("\n");
+        erro.append( err );
+    }
+    
+    
+    
+    
+    
+    
+=======
 public class Operacao {
 
 	private boolean status;
@@ -34,4 +87,5 @@ public class Operacao {
 
 	}
 
+>>>>>>> THEIRS
 }
