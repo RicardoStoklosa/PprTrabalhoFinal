a29b9b6d4f4a731955900dee1065fc90fc695036
RicardoStoklosa <stoklosa.ricardo@gmail.com>
2017-12-07 17:12:21 -0200

    testes

--- JavaApplicationBanco/src/EDA/Conta.java
+++ JavaApplicationBanco/src/EDA/Conta.java
@@ -1,21 +1,16 @@
 package EDA;
 
-public class Conta {
+public class Conta{
 
 	private double saldo;
 
-	private LocalDate tempoConta;
-
-	private Usuario usuario;
-
-	private Sacar sacar;
-
-	private Depositar depositar;
-
-	private Emprestimo emprestimo;
-
-	private Gerenciar gerenciar;
-
-	private Administrador administrador;
+        public Conta(double sald){
+            saldo =sald;
+        }
+	public double getSaldo(){
+            return saldo;
+        }
+        
+                
 
 }
